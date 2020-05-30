04c2e07c9cdf3a85313f262dd3508956fe8bc3e1
Vbj <ijalab1808@gmail.com>
2020-05-30 10:44:15 +0530

    abcd

--- Backend/app.js
+++ Backend/app.js
@@ -6,4 +6,4 @@ app.get('/', (req,res)=> res.send('Hello world'))
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log('Server running on port ${port} '));
+app.listen(port, ()=> console.log(`Server running on port ${port} `));
