5d0586d63565506f284eaee6ccc9140fa188a292
Vbj <ijalab1808@gmail.com>
2020-05-30 12:30:57 +0530

    REST API

--- Backend/routes/api/books.js
+++ Backend/routes/api/books.js
@@ -18,3 +18,23 @@ router.get('/:id', (req,res)=> {
         .catch(err => res.status(404).json({ nobookfound: 'No book found'}));
 });
 
+router.post('/', (req,res) => {
+    Book.create(req.body)
+        .then(body => res.json({msg: 'Book added successfully'}))
+        .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+});
+
+router.put('/:id', (req,res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(book => res.json({ msg: 'Updated successfully'}))
+        .catch(err => res.status(400).json({ error: 'Unable to update the database'}));
+});
+
+router.delete('/:id', (req,res) => {
+    Book.findByIdAndRemove(req.params.id, req.body)
+        .then(book => res.json({ msgs: 'Book entry deleted successfully'}))
+        .catch(err => res.status(404).json({error: 'No such a book'}));
+});
+
+module.exports = router;
+
