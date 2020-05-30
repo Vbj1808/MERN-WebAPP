7d375c85eb5cae5c5ff907d4f1b07dc4e36bc729
Vbj <ijalab1808@gmail.com>
2020-05-30 19:52:03 +0530

    modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const connection = require('./config/db');
+var cors = require('cors');
+
+
+const books = require('./routes/api/books');
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req,res)=> res.send('Hello world'))
 
-const port = process.env.PORT || 3000;
+app.use('/api/books',books);
+
+const port = process.env.PORT || 8082;
 
 app.listen(port, ()=> console.log(`Server running on port ${port} `));
