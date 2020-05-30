81637c595fd7470489c51f5e222356e97688882a
Vbj <ijalab1808@gmail.com>
2020-05-30 18:17:03 +0530

    modified main

--- FrontEnd/books/src/App.js
+++ FrontEnd/books/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import '../src/components/MainComponent'
+import Main from '../src/components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- FrontEnd/books/src/components/MainComponent.js
+++ FrontEnd/books/src/components/MainComponent.js
@@ -1,3 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import '../App.css';
+
+import CreateBook from './CreateBook';
+import DisplayBookList from './DisplayBookList';
+import DisplayBookDetails from './DisplayBookDetails';
+import UpdateBookInfo from './UpdateBookInfo';
+
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                <Route exact path ='/' component={DisplayBookList} />
+                <Route  path ='/create-book' component={CreateBook} />
+                <Route exact path ='/edit-book/:id' component={UpdateBookInfo} />
+                <Route exact path ='/show-book/:id' component={DisplayBookDetails} />       
+            </div>
+        );
+    }
+}
+
+export default Main;
